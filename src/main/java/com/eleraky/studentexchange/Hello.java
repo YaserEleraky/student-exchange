f55ab3e715f1e5c127bfbb2955125599f7a3a316
Eleraki <elsaydyasser@yahoo.com>
2026-05-07 18:39:01 +0500

    User Model with services And Controllers

--- src/main/java/com/eleraky/studentexchange/Hello.java
+++ src/main/java/com/eleraky/studentexchange/Hello.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/")
     public String hello(){
-        return "Hello World!";
+        return "Hello This My Student Exchange App!";
     }
 }
